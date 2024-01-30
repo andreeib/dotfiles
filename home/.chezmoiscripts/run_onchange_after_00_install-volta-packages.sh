293a52f442d5fd63e486260326d38736d5a341ed
Andrei Bejan <andreibbejan@gmail.com>
2024-01-30 23:29:18 +0200

    Fix volta install

--- home/.chezmoiscripts/run_onchange_after_00_install-volta-packages.sh
+++ home/.chezmoiscripts/run_onchange_after_00_install-volta-packages.sh
@@ -5,8 +5,7 @@ set -eufo pipefail
 echo "Installing Volta packages"
 
 # Add Volta to path in case it's not added yet
-VOLTA_HOME="$HOME/.volta"
-PATH="$VOLTA_HOME/bin:$PATH"
+eval $(/opt/homebrew/bin/brew shellenv)
 
 volta install node
 volta install npm
