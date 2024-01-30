56b67d2541048bfd618f5aebbf69d05c4f663d67
Andrei Bejan <andreibbejan@gmail.com>
2024-01-30 23:06:02 +0200

    Fix volta install

--- home/.chezmoiscripts/run_onchange_after_00_install-volta-packages.sh
+++ home/.chezmoiscripts/run_onchange_after_00_install-volta-packages.sh
@@ -4,6 +4,10 @@ set -eufo pipefail
 
 echo "Installing Volta packages"
 
+# Add Volta to path in case it's not added yet
+VOLTA_HOME="$HOME/.volta"
+PATH="$VOLTA_HOME/bin:$PATH"
+
 volta install node
 volta install npm
 volta install yarn
